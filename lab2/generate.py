038c283ddad54d1f03c95d9c453fa975316c8371
accce953 <i@jacob953.com>
2023-05-03 15:43:01 +0800

    fix: correct speedup rate & generate only one diagram
    Signed-off-by: accce953 <i@jacob953.com>

--- lab2/generate.py
+++ lab2/generate.py
@@ -16,8 +16,6 @@ with open('pi-mpi.out', 'r') as f:
 _, *pi_data = map(float, re.findall(r"[-+]?\d*\.\d+|\d+", pi_lines[0]))
 avg_time_pi = sum(pi_data) / COUNT
 
-print(pi_data)
-
 pi_mpi_data = {}
 for line in pi_mpi_lines:
     np_val, *times = map(float, re.findall(r"[-+]?\d*\.\d+|\d+", line))
@@ -25,22 +23,16 @@ for line in pi_mpi_lines:
 
 # 计算每个NP的平均Time和加速比
 np_vals = sorted(list(pi_mpi_data.keys()))
-avg_times_pi_mpi = []
 speedup = []
 for np_val in np_vals:
     avg_time_pi_mpi = sum(pi_mpi_data[np_val]) / COUNT
-    avg_times_pi_mpi.append(avg_time_pi_mpi)
-    speedup.append(avg_time_pi_mpi / avg_time_pi)
+    speedup.append(avg_time_pi / avg_time_pi_mpi)
 
 # 画图
-fig, axs = plt.subplots(2, 1, figsize=(8, 8))
-axs[0].plot(np_vals, avg_times_pi_mpi)
-axs[0].set_xlabel('NP')
-axs[0].set_ylabel('Cost Time of Multi Thread')
-
-axs[1].plot(np_vals, speedup)
-axs[1].set_xlabel('NP')
-axs[1].set_ylabel('Speedup(Multi Thread/Single Thread)')
+fig, axs = plt.subplots(1, 1, figsize=(8, 8))
+axs.plot(np_vals, speedup)
+axs.set_xlabel('NP')
+axs.set_ylabel('Speedup(Multi Thread/Single Thread)')
 
 plt.tight_layout()
 plt.show()
