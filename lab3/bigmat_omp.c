f5ee0aafd39f9370c28059b0110ebe5c17e5daf4
accce953 <i@jacob953.com>
2023-05-16 00:02:12 +0800

    feat: lab3 create bigmat_omp.c
    Signed-off-by: accce953 <i@jacob953.com>

---
+++ lab3/bigmat_omp.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define N 1000
+
+int NP = 1;
+int A[N][N], B[N][N];
+
+void matrixMultiply()
+{
+    int C[N][N];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            C[i][j] = 0;
+            for (int k = 0; k < N; k++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void init()
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            A[i][j] = i + j;
+            B[i][j] = i - j;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2)
+    {
+        NP = atoi(argv[1]);
+    }
+
+    init();
+    omp_set_num_threads(NP);
+
+    double elapsedTime;
+    elapsedTime -= omp_get_wtime();
+    matrixMultiply();
+    elapsedTime += omp_get_wtime();
+    printf("Multiply %dx%d matrix...\nNP=%d Time=%.6lf", N, N, NP, elapsedTime);
+
+    return 0;
+}
